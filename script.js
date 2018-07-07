485d75c026830b7e7170465a63c27b60464894e9
Neha Chauhan <nehachauhan79@gmail.com>
2018-07-08 00:56:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function opencard(){
+    // document.getElementById("outside").style.visibility = "collapse";
+    //  document.getElementById("inside").style.visibility = "visible";
+     document.getElementById("outside").className = "opencard";
+
+
+}
+
+
+function closecard(){
+    // document.getElementById("inside").style.visibility= "collapse";
+    // document.getElementById("outside").style.visibility = "visible";
+    document.getElementById("outside").className = "";
+   
+
+}
